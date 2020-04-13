1076948fbf34ad83bfdfa6706609694ff06613c7
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-13 18:32:13 -0500

    [Fixing]: Different approach

--- 0x00-csharp-hello_world/5-print_float/5-print_float.cs
+++ 0x00-csharp-hello_world/5-print_float/5-print_float.cs
@@ -1,9 +1,10 @@
 ﻿using System;
+
 class Program
 {
 	static void Main(string[] args)
         {
 		float number = 3.14159f;
-		Console.WriteLine("Float: {0}", number.ToString("F"));
+		Console.WriteLine("Float: {0:0.00}", number);
         }
 }
