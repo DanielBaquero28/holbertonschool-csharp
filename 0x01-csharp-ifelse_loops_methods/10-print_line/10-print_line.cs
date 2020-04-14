21a427fa92128686bd44333292966a2f87a25577
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-14 15:37:24 -0500

    [Feature]: Testing

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,14 @@
+﻿using System;
+class Line
+{
+    public static void PrintLine(int Length)
+    {
+        int i;
+
+        for (i = 0; i <= Length; i++)
+        {
+            Console.Write("_");
+        }
+        Console.Write("\n");
+    }
+}
