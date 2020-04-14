f840c18cea72385d2de4a2b836fe49a7417ae32e
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-14 12:07:19 -0500

    [Fixing]: Tried not saving it on variable

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -5,13 +5,12 @@ class Program
     {
         Random rndm = new Random();
         int number = rndm.Next(-10000, 10000);
-        int lastDigit = number % 10;
-        Console.Write("The last digit of {0} is {1} ", number, lastDigit);
-        if (lastDigit > 5)
+        Console.Write("The last digit of {0} is {1} ", number, number % 10);
+        if ((number % 10) > 5)
         {
             Console.WriteLine("and is greater than 5");
         }
-        else if (lastDigit == 0)
+        else if ((number % 10) == 0)
         {
             Console.WriteLine("and is 0");
         }
