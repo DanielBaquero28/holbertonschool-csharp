8824736caa03b62dd5358738262aafb17946a99f
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-14 15:07:12 -0500

    [Feature]: Testing if Abs is right

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int tmpNumber;
+
+        tmpNumber = number % 10;
+        if (tmpNumber < 0)
+        {
+            tmpNumber = Math.Abs(tmpNumber);
+        }
+        Console.Write("{0}", tmpNumber);
+
+        return (tmpNumber);
+    }
+}
