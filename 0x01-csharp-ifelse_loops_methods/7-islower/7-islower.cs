70a962da7a1fb9a1a605df0db6779705e22fdd94
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-14 14:52:55 -0500

    [Feature]: Converted to Ascii Decimals

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        int asciiVal = (int)Convert.ToChar(c);
+        if (asciiVal >= 97 && asciiVal <= 122)
+        {
+            return(true);
+        }
+        else
+        {
+            return(false);
+        }
+    }
+}
