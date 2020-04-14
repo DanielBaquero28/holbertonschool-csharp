07da6b369652f7ebe40e2631ccce8bc6d9978deb
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-14 16:14:20 -0500

    [Feature]: Printing Diagonal

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Line
+{
+    public static void PrintDiagonal(int Length)
+    {
+        int i, j;
+        for (i = 0; i < Length; i++)
+        {
+            for (j = 0; j < i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        Console.Write("\n");
+    }
+}
