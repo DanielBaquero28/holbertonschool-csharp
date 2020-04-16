153b8bbd8b6e22399dfa5954ac8d6a785ac4ea56
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 15:57:21 -0500

    [Fixing]: Tryiong with different approach

--- 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,34 +1,35 @@
 ﻿using System;
+
 class Array
 {
     public static int[] CreatePrint(int size)
     {
-        int i;
-        var arr = new int[size];
         if (size < 0)
         {
-            System.Console.WriteLine("Size cannot be negative");
+            Console.WriteLine("Size cannot be negative");
             return (null);
         }
 
+        var arr = new int[size];
         if (size == 0)
         {
-            System.Console.WriteLine();
+        Console.WriteLine();
             return (arr);
         }
 
-        for (i = 0; i < size; i++)
+        for(int i = 0; i < size; i++)
         {
             if (i < size - 1)
             {
-                System.Console.Write("{0} ", i);
+                Console.Write("{0} ", i);
             }
             else
             {
-                System.Console.WriteLine(i);
+                Console.Write(i);
             }
             arr[i] = i;
         }
+        Console.WriteLine();
         return (arr);
     }
 }
