127b6eb8a03b3b297a533613e0682d94becbe9d5
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 18:39:40 -0500

    [Feature]: Test v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList.Count == 0)
+        {
+            return (null);
+        }
+
+        List<bool> evenList = new List<bool>();
+
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (myList[i] % 2 == 0)
+            {
+                evenList.Add(true);
+            }
+            else
+            {
+                evenList.Add(false);
+            }
+        }
+        return (evenList);
+    
+    }
+}
