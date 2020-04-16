4f5b15a6cde1e7b5cbad00cc3d79889fbb358212
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 17:15:30 -0500

    [Feature]: Test v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+        }
+        else
+        {
+            int len = array.Length;
+            int[] revArr = new int[len];
+            for (int i = 0, j = len - 1; i < len; i++, j--)
+            {
+                revArr[i] = array[j];
+                if (i < len - 1)
+                {
+                    Console.Write("{0} ", revArr[i]);
+                }
+                else
+                {
+                    Console.WriteLine("{0}", revArr[i]);
+                }
+            }
+        }
+    }
+}
