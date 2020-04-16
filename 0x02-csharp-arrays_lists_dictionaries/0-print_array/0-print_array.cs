906c7b17b72746d08cf441125a558cbe25d5714b
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 15:08:33 -0500

    [Feature]: Testing v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        int i;
+        int[] arr = new int[size];
+
+        if (size < 0)
+        {
+            System.Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+
+        for (i = 0; i < size - 1; i++)
+        {
+            System.Console.Write("{0} ", i);
+        }
+        System.Console.WriteLine("{0}", i);
+        return (arr);
+    }
+}
