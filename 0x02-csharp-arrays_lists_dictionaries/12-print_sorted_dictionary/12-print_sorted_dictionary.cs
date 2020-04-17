e7d0def9f566b59ad77d62db5a8091ef79b7e481
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 20:46:42 -0500

    [Feature]: Used SortedList

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        SortedList<string, string> sortedDict = new SortedList<string, string>(myDict);
+        foreach (KeyValuePair<string, string> item in sortedDict)
+        {
+            Console.WriteLine("{0}: {1}", item.Key, item.Value);
+        }
+    }
+}
