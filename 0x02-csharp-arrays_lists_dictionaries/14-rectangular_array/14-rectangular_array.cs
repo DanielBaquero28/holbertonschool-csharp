7e82845620eb9f03571f52c1049f2d16de5da6c1
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 21:22:17 -0500

    [Feature]: Test v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[,] grid = new int[5,5];
+        grid[2,2] = 1;
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                if (j < 4)
+                    Console.Write("{0} ", grid[i, j]);
+                
+                else
+                    Console.WriteLine("{0}", grid[i, j]);
+   
+            }
+        }
+    }
+}
+
+
