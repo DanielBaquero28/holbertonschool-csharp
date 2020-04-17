307824ef53ed0156afe7b333380433c2da53be9d
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 19:02:05 -0500

    [Feature]: Test v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keys = 0;
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            keys += 1;
+        }
+
+        return (keys);
+    }
+}
