b61f4be32ce23573c48db2c1b33547dc5fda9643
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-16 21:05:47 -0500

    [Feature]: Test v0

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        int maxVal = 0;
+        string key = String.Empty;
+        if (myList.Count == 0)
+        {
+            return ("None");
+        }
+        foreach(KeyValuePair<string, int> item in myList)
+        {
+            if (item.Value > maxVal)
+            {
+                maxVal = item.Value;
+                key = item.Key;
+            }
+            else
+            {
+                continue;
+            }
+        }
+        return (key);
+
+    }
+}
