6bb3ae16c744a68abd5e62d4ffbcb9b2827c3b32
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 14:03:19 -0500

    [Feature]: Used SortedSet collection

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        var hashSet1 = new HashSet<int>(list1);
+        var hashSet2 = new HashSet<int>(list2);
+        hashSet1.IntersectWith(hashSet2);
+
+        var sortedHash = new SortedSet<int>(hashSet1);
+        var newList = new List<int>(sortedHash);
+
+        return(newList);
+    }
+}
