6bbe4cebab3841e50b19eb71ec07d57e85484e3e
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 16:37:58 -0500

    [Feature]: Testing v0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newList = new LinkedList<int>();
+        if (size < 0)
+        {
+            return(newList);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            newList.AddLast(i);
+        }
+        foreach(int number in newList)
+        {
+            Console.WriteLine(number);
+        }
+        return(newList);
+    }
+}
