3bc415662df284a8b2de69df8e3fc30872a4550a
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 17:34:29 -0500

    [Feature]: Testing v0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int i = 0;
+        int index = 0;
+        if (myLList.Contains(value))
+        {
+            foreach (int val in myLList)
+            {
+                if (val == value)
+                {
+                    index = i;
+                }
+                else
+                {
+                    i += 1;
+                }
+            }
+        }
+        else
+        {
+            return(-1);
+        }
+        return(index);
+    }
+}
