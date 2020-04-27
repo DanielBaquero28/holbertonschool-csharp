f07a8789ba65e35c89916fc17eb32bb2dc8a18cd
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 16:19:02 -0500

    [Feature]: Queue data structure

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int count = aQueue.Count;
+        Console.WriteLine("Number of items: {0}", count);
+        if (count <= 0)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            string topQueue = aQueue.Peek();
+            Console.WriteLine("First item: {0}", topQueue);
+        }
+        aQueue.Enqueue(newItem);
+        if (aQueue.Contains(search))
+        {
+            Console.WriteLine("Queue contains \"{0}\": True", search);
+            while (aQueue.Contains(search))
+            {
+                aQueue.Dequeue();
+            }
+        }
+        else
+        {
+            Console.WriteLine("Queue contains \"{0}\": False");
+        }
+        return(aQueue);
+    }
+}
