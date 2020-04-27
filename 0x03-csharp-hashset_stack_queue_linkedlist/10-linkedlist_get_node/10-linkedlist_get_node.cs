f0d2b095cd8415c01ebe403026f65f9db5202415
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 18:21:19 -0500

    [Feature]: Testing checkers

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        
+        if (n > myLList.Count)
+        {
+            return(0);
+        }
+        int i = 0;
+        foreach (int value in myLList)
+        {
+            if (i == n)
+            {
+                return(value);
+            }
+            i += 1;
+        }
+        return (0);
+    }
+}
