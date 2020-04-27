e3ec4e9085e66d64c3abf28b3343e870566182cb
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 13:37:45 -0500

    [Feature]: Converted HashSet to remove duplicates

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sumVal = 0;
+        var mySet = new HashSet<int>(myList);
+        foreach (int element in mySet)
+        {
+            sumVal += element;
+        }
+        return(sumVal);
+
+    }
+}
