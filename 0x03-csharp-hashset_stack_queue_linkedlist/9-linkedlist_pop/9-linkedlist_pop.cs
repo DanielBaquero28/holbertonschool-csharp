10985e9b44eb11d388e3a3561f98b75b43a091e0
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 18:02:18 -0500

    [Feature]: Used RemoveFirst()

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count <= 0)
+        {
+            return(-1);
+        }
+        
+        LinkedListNode<int> headNode = new LinkedListNode<int>(myLList.First.Value);
+        myLList.RemoveFirst();
+        return(headNode.Value);
+    }
+}
