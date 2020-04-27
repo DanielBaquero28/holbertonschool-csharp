6048237e8bfbb2527a3b3489b136f5c1f4938919
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 18:27:09 -0500

    [Feature]: Addind LinkedList nodes

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myList)
+    {
+        int sumValue = 0;
+        foreach (int val in myList)
+        {
+            sumValue += val;
+        }
+        return(sumValue);
+    }
+}
