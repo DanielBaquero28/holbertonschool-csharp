f9d2494393e33612c97ef0d53b653ea43cec9785
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 14:20:58 -0500

    [Feature]: Used ExceptWith() and SortedSet()

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        var hashSet1 = new HashSet<int>(list1);
+        var hashSet2 = new HashSet<int>(list2);
+        var tmpSet = new HashSet<int>(hashSet1);
+
+        hashSet1.ExceptWith(hashSet2);
+        hashSet2.ExceptWith(tmpSet);
+        hashSet1.UnionWith(hashSet2);
+
+        var sortSet = new SortedSet<int>(hashSet1);
+        var myList = new List<int>(sortSet);
+
+        return(myList);
+
+    }
+}
