219abf00d395f6db09eda05bdd70442746fd5873
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-28 17:36:18 -0500

    [Feature]: Overrode ToString()

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string Name, float Age, string Owner, Rating Rat)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rat;
+        
+    }
+
+    public override string ToString()
+    {
+        return("Dog Name: " + name + "\nAge: " + age + "\nOwner: " + owner + "\nRating: " + rating + "\n");
+    }
+    
+}
