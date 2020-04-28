36a67a39c9a65368a0829772124cc3ab91ac251f
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 19:12:39 -0500

    [Feature]: Inserted Node in an ordered LList

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        int value = 0;
+        foreach (int val in myLList)
+        {
+            if (n >= val)
+            {
+                continue;
+            }
+            else
+            {
+                value = val;
+                break;
+            }
+        }
+        LinkedListNode<int> tmpNode = myLList.Find(value);
+        LinkedListNode<int> newNode = myLList.AddBefore(tmpNode, n);
+        return(newNode);
+    }
+}
