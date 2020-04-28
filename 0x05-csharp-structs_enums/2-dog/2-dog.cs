edb8c160fabec497f7a925ceda37e5449ab504c4
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-28 17:22:07 -0500

    [Feature]: Added parameters to the enumeration

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+enum Rating
+{
+    Good,
+    Great,
+    Excellent
+}
+
+struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+    public Dog(string Name, float Age, string Owner, Rating Rat)
+    {
+        name = Name;
+        age = Age;
+        owner = Owner;
+        rating = Rat;
+    }
+    
+}
