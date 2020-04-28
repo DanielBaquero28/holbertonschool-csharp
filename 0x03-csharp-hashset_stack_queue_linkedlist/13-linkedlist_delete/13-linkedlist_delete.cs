a7181a5007e7bb96c8747bd224887f967295a882
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-27 20:24:54 -0500

    [Feature]: Remove node at index

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        if (index >= myLList.Count)
+        {
+            return;
+        }
+        LinkedListNode<int> tmpNode = myLList.First;
+        int i = 0;
+        while (tmpNode != null)
+        {
+            if (i == index)
+            {
+                myLList.Remove(tmpNode);
+                return;
+            }
+            else
+            {
+                tmpNode = tmpNode.Next;
+                i += 1;
+            }
+        }
+    }
+}
