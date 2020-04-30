9e887233372bc537e62522529aa89e658f05247a
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-30 15:16:08 -0500

    [Feature]: Awesome topic

---
+++ 0x06-csharp-classes/5-enemy/5-enemy.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Field that keeps track of the health
+        /// </summary>
+        private int health;
+        /// <summary>
+        /// Name Field
+        /// </summary>
+        private string name = "(No name)";
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Overloading with a new class constructor
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        /// <summary>
+        /// Retrives the value of health
+        /// </summary>
+        /// <returns>
+        /// Health's value
+        /// </returns>
+        public int GetHealth() => (this.health);
+
+        /// <summary>
+        /// Property that retrieves and updates name
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return(this.name);
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// Overrides ToString() in order to print the Zombie object's attributes
+        /// </summary>
+        public override string ToString()
+        {
+            return("Zombie name: " + this.name + " / Total Health: " + this.health);
+        }
+    }
+}
