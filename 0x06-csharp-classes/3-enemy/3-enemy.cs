ef0a0a23f869a8d87447ec9db401198627efcce0
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-30 14:49:57 -0500

    [Feature]: Retrieving health's value

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Field that keeps track of the health
+        /// </summary>
+        private int health;
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Overloading with a new class constructor
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+        /// <summary>
+        /// Retrives the value of health
+        /// </summary>
+        /// <returns>
+        /// Health's value
+        /// </returns>
+        public int GetHealth() => (this.health);
+    }
+}
