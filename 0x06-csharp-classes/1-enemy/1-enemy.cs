5ab11c1f2013bd582f045446177cf7c09e69c353
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-30 14:25:00 -0500

    [Feature]: Better understanding on comments

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Field that keeps track of the health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+    }
+}
