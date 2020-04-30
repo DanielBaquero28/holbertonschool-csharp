890656f44a362941f4037e2a6073bad2ae4c3754
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-04-30 14:33:35 -0500

    [Feature]: Testing v0

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>
+    /// Empty class
+    /// </summary>
+    public class Zombie
+    {
+        /// <summary> 
+        /// Field that keeps track of the health
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Class Constructor
+        /// </summary>
+        public Zombie()
+        {
+            this.health = 0;
+        }
+
+        /// <summary>
+        /// Overloading with a new class constructor
+        /// </summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            else
+            {
+                this.health = value;
+            }
+        }
+    }
+}
