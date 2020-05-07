b3120981a8a8436c2ef8d62a6134f9a44f3a9435
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-07 13:15:30 -0500

    [Feature]: Testing v0

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary> Object Class </summary>
+class Obj
+{
+    /// <summary> Finds out if derived class inherits exactly by base class </summary>
+    /// <returns> true if it is, else false </returns>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+        {
+            return(true);
+        }
+
+        return (false);
+    }
+}
