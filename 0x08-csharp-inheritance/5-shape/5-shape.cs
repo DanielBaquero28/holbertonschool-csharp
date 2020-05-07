5f31aff833f6f45d53cec96641d21fba1afdf031
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-07 14:38:02 -0500

    [Feature]: Testing v0

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+ 
+/// <summary> Shape Base Class </summary>
+public class Shape
+{
+    /// <summary> Area Method, thows an exception </summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
