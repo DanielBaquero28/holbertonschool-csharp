61493aaf0909200b76ac865b89824ab53306f1d0
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-07 14:07:50 -0500

    [Feature]: Testing v0

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary> Object Class </summary>
+class Obj
+{
+    /// <summary> Prints the names of the available properties and methods of an object.</summary>
+    /// <returns> Nothing </returns>
+    public static void Print(object myObj)
+    {
+        var t = myObj.GetType().GetTypeInfo();
+
+        // Properties Info
+        PropertyInfo[] propertiesInfo = t.GetProperties();
+        System.Console.WriteLine("{0} Properties:", t.Name);
+        for (int i = 0; i < propertiesInfo.Length; i++)
+        {
+            System.Console.WriteLine(propertiesInfo[i]);
+        }
+
+        // Methods Info
+        MethodInfo[] methodInfo = t.GetMethods();
+        System.Console.WriteLine("{0} Methods:", t.Name);
+        for (int i = 0; i < methodInfo.Length; i++)
+        {
+            System.Console.WriteLine(methodInfo[i]);
+        }
+
+    }
+}
