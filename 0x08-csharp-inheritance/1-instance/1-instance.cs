8ab394c8bb740f5f9afdad37bf887f5cb8c4a1ac
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-07 13:05:14 -0500

    [Feature]: Testing v0

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+/// <summary> Object Class </summary>
+class Obj
+{
+    /// <summary> Finds out if an object is an instance of Array or inherited from a class Array </summary>
+    /// <returns> true if it is, else false </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (typeof(Array).IsInstanceOfType(obj))
+        {
+            return (true);
+        }
+
+        return (false);
+    }
+}
