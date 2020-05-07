edf14a4fadb62b799081033e2d7876d996a38bbb
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-07 12:42:35 -0500

    [Fixed]: Was missing XML comments

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+
+/// <summary> Object Class </summary>
 class Obj
 {
+    /// <summary> Finds out if obj is of type int </summary>
+    /// <returns> true if it is, else false </returns>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int)
