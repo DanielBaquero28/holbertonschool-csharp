bac52ccc042c9af7704e8c4739699ab66291f303
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-27 16:38:30 -0500

    [Feature]: Testing v0

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/// <summary> VectorMath Class </summary>
+class VectorMath
+{
+    /// <summary> Finds magnitude (length) of a vector </summary>
+    public static double Magnitude(double[] vector)
+    {
+        double mag = 0;
+
+        if (vector.Length <= 1 || vector.Length > 3)
+        {
+            return (-1);
+        }
+        
+        for (int i = 0; i < vector.Length; i++)
+        {
+            mag += vector[i] * vector[i];
+        }
+
+        mag = Math.Sqrt(mag);
+
+        return (Math.Round(mag, 2));
+    }
+}
