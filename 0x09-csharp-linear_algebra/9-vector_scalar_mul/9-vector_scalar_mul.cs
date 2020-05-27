69b6442773637e6357b8ecddbe11171542966e8e
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-27 17:24:49 -0500

    [Feature]: Testing v2

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+/// <summary> VectorMath Class </summary>
+class VectorMath
+{
+    /// <summary> Multiplies a vector by a scalar </summary>
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        if (vector.Length <= 1 || vector.Length > 3)
+        {
+            return (-1);
+        }
+
+        double[] r_vector = new double[vector.Length];
+        for (int i = 0; i < vector.Length; i++)
+        {
+            r_vector[i] = vector[i] * scalar;
+        }
+
+        return (r_vector);
+    }
+}
