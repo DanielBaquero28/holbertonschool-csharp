7a2969f6fc6b4ed199e85a7a23be10cf39ffc072
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-27 21:55:40 -0500

    [Feature]: Testing v5

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+/// <summary> MatrixMath Class </summary>
+class MatrixMath
+{
+    /// <summary> Rotate a 2D matrix by angle in radians </summary>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double[,] f_matrix = {{-1}};
+        double[,] r_matrix = new double[2, 2];
+
+        if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
+        {
+            return (f_matrix);
+        }
+
+        double[,] rotate_matrix = {{Math.Cos(angle), Math.Sin(angle)}, {Math.Sin(angle) * -1, Math.Cos(angle)}};
+        double result = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                result = 0;
+                for (int k = 0; k < matrix.GetLength(1); k++)
+                {
+                    result += Math.Round(matrix[i, k] * rotate_matrix[k, j], 2);
+                }
+
+                r_matrix[i, j] = result;
+            }
+        }
+
+        return (r_matrix);
+    }
+}
