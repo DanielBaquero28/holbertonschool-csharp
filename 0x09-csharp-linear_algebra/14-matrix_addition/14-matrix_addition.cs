63f175d94b6441ffb9fe51ef14beafac377ffe08
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-27 19:01:18 -0500

    [Fixing]: Testing another way

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -6,31 +6,35 @@ class MatrixMath
     /// <summary> Adds two matrixes </summary>
     public static double[,] Add(double[,] matrix1, double[,] matrix2)
     {
-        double[,] f_matrix = new double[] {{ -1 }};
-        if (matrix1.GetLength(0) <= 1 || matrix1.GetLength(0) > 3 || matrix1.GetLength(1) <= 1 || matrix1.GetLength(1) > 3)
+        double[,] f_matrix = {{-1}};
+        double[,] r_matrix;
+        int mx1 = matrix1.GetLength(0);
+        int my1 = matrix1.GetLength(1);
+        int mx2 = matrix2.GetLength(0);
+        int my2 = matrix2.GetLength(1);
+
+        if (mx1 == 2 && my1 == 2 && mx1 == mx2 && my1 == my2)
+        {
+            r_matrix = new double[2, 2];
+        }
+        else if (mx1 == 3 && my1 == 3 && mx1 == mx2 && my1 == my2 )
+        {
+            r_matrix = new double[3, 3];
+        }
+        else
         {
             return (f_matrix);
         }
 
-        if (matrix2.GetLength(0) <= 1 || matrix2.GetLength(0) > 3 || matrix2.GetLength(1) <= 1 || matrix2.GetLength(1) > 3)
-        {
-            return (f_matrix);
-        }
-
-        if ((matrix1.GetLength(0) != matrix2.GetLength(0)) || matrix1.GetLength(1) != matrix2.GetLength(1))
-        {
-            return (f_matrix);
-        }
-
-        double[,] r_matrix = new double[matrix2.GetLength(0), matrix2.GetLength(1)];
         for (int i = 0; i < matrix1.GetLength(0); i++)
         {
             for (int j = 0; j < matrix1.GetLength(1); j++)
             {
-                r_matrix[i][j] = matrix1[i][j] + matrix2[i][j];
+                r_matrix[i, j] = matrix1[i, j] + matrix2[i, j];
             }
 
-            return (r_matrix);
         }
+
+        return (r_matrix);
     }
 }
