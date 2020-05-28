d15c5929e135d0ccc0d26d867aebf4d6d00e289f
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-05-27 22:18:15 -0500

    [Feature]: Testing v5

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary> MatrixMath Class </summary>
+class MatrixMath
+{
+    /// <summary> Transposes a given matrix </summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] r_matrix = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(1); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(0); j++)
+            {
+                r_matrix[i, j] = matrix[j, i];
+            }
+        }
+
+        return (r_matrix);
+    }
+}
