b9cf9e6671d80b6c947252a123917822166ccb64
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-27 14:24:51 -0500

    [Fix]: Code Style

--- 0x0A-csharp-generics/0-queue/0-queue.cs
+++ 0x0A-csharp-generics/0-queue/0-queue.cs
@@ -3,9 +3,9 @@
 /// <summary> Class Queue of type T </summary>
 class Queue<T>
 {
-    /// <summary> Checks the Type of Queue </summary>
-    public Type CheckType()
-    {
-        return (typeof(T));
-    }
+        /// <summary> Checks the Type of Queue </summary>
+        public Type CheckType()
+        {
+                return (typeof(T));
+        }
 }
