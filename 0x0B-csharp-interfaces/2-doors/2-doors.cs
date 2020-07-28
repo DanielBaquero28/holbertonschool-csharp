5567a5e3330159e689ced323a9c01e8afa52cb09
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-28 15:16:39 -0500

    [Feature]: Testing v0

---
+++ 0x0B-csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+/// <summary> Base Class from which other Classes derive from. </summary>
+public abstract class Base
+{
+    /// <summary> Name Variable </summary>
+    public string name;
+
+    /// <summary> Overrides ToString() to desired result</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+}
+
+/// <summary> Interactive interface </summary>
+interface IInteractive
+{
+    void Interact();
+}
+
+/// <summary> Breakable Interface </summary>
+interface IBreakable
+{
+    int durability { get; set; }
+    void Break();
+}
+
+/// <summary> Collectable Interface </summary>
+interface ICollectable
+{
+    bool isCollected { get; set; }
+    void Collect();
+}
+
+/// <summary> Door class which handles Door actions </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary> Door Constructor </summary>
+    public Door(string _name)
+    {
+        if (_name == null)
+            this.name = "Door";
+        else
+            this.name = _name;
+    }
+
+    /// <summary> Prints a message when the door interacts with something </summary>
+    public void Interact()
+    {
+         System.Console.WriteLine("You try to open the " + this.name + ". It's locked.");
+    }
+}
