1ba4a5d6deac06fd36be2c0c52b6d39261614045
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-28 16:22:34 -0500

    [Fix]: Testing v3

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -70,17 +70,23 @@ public class Decoration : Base, IInteractive, IBreakable
     /// <summary> Prints a message regarding the decoration of the door </summary>
     public void Interact()
     {
-        if (this.durability <= 0) System.Console.WriteLine("The {0} has been broken.", this.name);
-        else if (this.isQuestItem == true) System.Console.WriteLine("You look at the {0}. There's a key inside.", this.name);
-        else if (this.isQuestItem == false) System.Console.WriteLine("You look at the {0}. Not much to see here.", this.name);
+        if (this.durability <= 0)
+            System.Console.WriteLine("The {0} has been broken.", this.name);
+        else if (this.isQuestItem == true)
+            System.Console.WriteLine("You look at the {0}. There's a key inside.", this.name);
+        else
+            System.Console.WriteLine("You look at the {0}. Not much to see here.", this.name);
     }
 
     /// <summary> Handles the Break actions when they take place </summary>
     public void Break()
     {
         this.durability--;
-        if (this.durability > 0) System.Console.WriteLine("You hit the {0}. It cracks.", this.name);
-        if (this.durability == 0) System.Console.WriteLine("You smash the {0}. What a mess.", this.name);
-        if (this.durability < 0) System.Console.WriteLine("The {0} is already broken.", this.name);
+        if (this.durability > 0)
+            System.Console.WriteLine("You hit the {0}. It cracks.", this.name);
+        if (this.durability == 0)
+            System.Console.WriteLine("You smash the {0}. What a mess.", this.name);
+        if (this.durability < 0)
+            System.Console.WriteLine("The {0} is already broken.", this.name);
     }
 }
