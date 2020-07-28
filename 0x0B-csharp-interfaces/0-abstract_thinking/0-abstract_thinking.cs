74051ad677cb58d598c4508389a6f1545ff210b2
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-28 14:35:42 -0500

    [Feature]: Overriding ToString()

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+/// <summary> Base Class from which other Classes derive from. </summary>
+abstract class Base
+{
+    public string name{ get; set; }
+
+    /// <summary> Overrides ToString() to desired result</summary>
+    public override string ToString()
+    {
+        return (this.name + " is a " + this.GetType().ToString());
+    }
+    
+}
