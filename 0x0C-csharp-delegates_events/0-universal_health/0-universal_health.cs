e5a0d3f61e7de59cee5c23b9f2d6140c38dc162d
Daniel Alejandro Baquero <835@holbertonschool.com>
2020-07-29 14:38:06 -0500

    [Feature]: Added Player Constructor

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/// <summary> Player Class which handles the Player attributes and actions </summary>
+public class Player
+{
+    string name;
+    float maxHp;
+    float hp;
+
+    /// <summary> Player's Constructor </summary>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        this.name = name;
+        if (maxHp > 0) this.maxHp = maxHp;
+        else
+        {
+            this.maxHp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.hp = maxHp;
+    }
+
+    /// <summary> Prints the Player's current health </summary>
+    public void PrintHealth()
+    {
+        System.Console.WriteLine("{0} has {1} / {2} health.", this.name, this.hp, this.maxHp);
+    }
+}
